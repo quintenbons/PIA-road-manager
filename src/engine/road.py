1193fc6ddba7403b84e54bb754615a2224a56a90
Luca Bitaudeau <luca.bitaudeau@grenoble-inp.org>
2023-10-19 09:39:48 +0200

    debut de modification

--- src/engine/road.py
+++ src/engine/road.py
@@ -1,14 +1,25 @@
 import numpy as np
 from dataclasses import dataclass
 from .types import Coordinate
-from typing import Optional
-
+from typing import Optional, List
+from .movable import Movable
 
 @dataclass
 class Road:
     start: Coordinate
     end: Coordinate
+    speedLimit: int
+    numberOfLane: int
     _norm: Optional[Coordinate] = None
+    _trafficFlow: float = None
+    _avgSpeed: float = None
+
+    content: List[Movable] = []
+
+    def update(self) -> None:
+        for m in self.content:
+            m.update()
+        
 
     def norm(self) -> Coordinate:
         if self._norm is None:
