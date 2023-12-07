a957da3092705fc74faa41944ca94c56a9d20f58
Clément Juventin <clementjuventin@gmail.com>
2023-12-07 11:09:00 +0100

    cross_duplex_strategy + strategy mutator

--- src/engine/strategies/cross_duplex_strategy.py
+++ src/engine/strategies/cross_duplex_strategy.py
@@ -1,33 +1,37 @@
 from ..traffic.traffic_light import TrafficLight
 from ..node import Node
 from typing import List
-from strategy import Strategy
+from .strategy import Strategy
 import math
 
 class TrafficGroup:
     traffic_lights: set[TrafficLight]
-    pos: tuple(float, float)
+    pos: tuple[float, float]
 
     def __init__(self):
         self.traffic_lights = set()
-        self.pos = None
+        self.pos = (None, None)
 
     def add_traffic_light(self, traffic_light: TrafficLight):
         self.traffic_lights.add(traffic_light)
         if self.pos is None:
             self.pos = traffic_light.pos
         else:
+            # This is wrong code, it should set the position to the center of all traffic lights, we need to discuss together to find the correct way to do it
             self.pos = (0, 0)
+            x = 0
+            y = 0
             for traffic_light in self.traffic_lights:
-                self.pos[0] += traffic_light.pos[0]
-                self.pos[1] += traffic_light.pos[1]
-            self.pos[0] /= len(self.traffic_lights)
-            self.pos[1] /= len(self.traffic_lights)
+                x += traffic_light.pos[0]
+                y += traffic_light.pos[1]
+            x /= len(self.traffic_lights)
+            y /= len(self.traffic_lights)
+            self.pos = (x, y)
 
 
 class CrossDuplexStrategy(Strategy):
     traffic_lights_group: List[TrafficGroup]
-    node_pos: tuple(float, float)
+    node_pos: tuple[float, float]
     opposit_degree_treshold :int= 15
 
     # 
@@ -37,8 +41,24 @@ class CrossDuplexStrategy(Strategy):
         self.traffic_lights_group = []
         self.node_pos = node.position
 
+        # print("--------- node pos -----------", self.node_pos)
+        # print("traffic lights")
+        # i = 0
+        # for traffic_light in self.trafficLights:
+        #     print(i, traffic_light.pos)
+        #     i += 1
+
         for traffic_light in self.trafficLights:
             self.associate_to_group(traffic_light)
+
+        # print("traffic lights group")
+        # i = 0
+        # for group in self.traffic_lights_group:
+        #     print(i, group.pos)
+        #     for traffic_light in group.traffic_lights:
+        #         print("    ", traffic_light.pos)
+        #     i += 1
+        # print("-------------------------------")
 
     def associate_to_group(self, traffic_light: TrafficLight):
         for group in self.traffic_lights_group:
@@ -47,44 +67,17 @@ class CrossDuplexStrategy(Strategy):
                 return
         group = TrafficGroup()
         group.add_traffic_light(traffic_light)
+        self.traffic_lights_group.append(group)
 
-    def is_opposit(self, center:tuple(float, float), a:tuple(float, float), b:tuple(float, float)) -> bool:
-        print(a, b)
-        print(get_angle(center, a), get_angle(center, b))
-        return abs(get_angle(center, a) - get_angle(center, b)) > self.opposit_degree_treshold
+    def is_opposit(self, center:tuple[float, float], a:tuple[float, float], b:tuple[float, float]) -> bool:
+        return abs(self.get_angle(center, a) % 180 - self.get_angle(center, b) % 180) < self.opposit_degree_treshold
     
-    def get_angle(self, center:tuple(float, float), point:tuple(float, float)) -> float:
+    def get_angle(self, center:tuple[float, float], point:tuple[float, float]) -> float:
         x, y = point[0] - center[0], point[1] - center[1]
         angle_rad = math.atan2(y, x)
         angle_deg = math.degrees(angle_rad)
         return angle_deg
     
-    def getAngle(center:tuple(float, float), point:tuple(float, float)) -> float:
-        x = point[0] - center[0]
-        y = point[1] - center[1]
-        if x == 0:
-            if y > 0:
-                return 90
-            else:
-                return 270
-        elif y == 0:
-            if x > 0:
-                return 0
-            else:
-                return 180
-        else:
-            angle = math.atan(y/x)
-            angle = math.degrees(angle)
-            if x > 0 and y > 0:
-                return angle
-            elif x < 0 and y > 0:
-                return 180 + angle
-            elif x < 0 and y < 0:
-                return 180 + angle
-            else:
-                return 360 + angle
-
-        
     def set_corridor(self, index: int):
         if index < len(self.trafficLights):
             self.corridor = self.trafficLights[index]
