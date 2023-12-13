36bda76a66b53ee0a774663a8e91bd251f4e9f1d
quintenbons <quintenbons@outlook.com>
2023-12-14 00:21:45 +0100

    Parallel generation (not yet seeded correctly)

--- src/gen_dataset.py
+++ src/gen_dataset.py
@@ -1,42 +1,30 @@
 #!/usr/bin/python3
-import signal
-import sys
 from ai.dataset import NodeDataset, score_tester
 import argparse
+import os
 
-global_dataset = None
+def generate_dataset(size: int, dest: os.PathLike):
+    ds = NodeDataset.from_generation(size, tqdm_disable=False)
+    print(f"Generated {len(ds)} entries")
+
+    for entry in ds:
+        print(entry)
+
+    ds.save(dest)
 
 def main():
-    global global_dataset
     parser = argparse.ArgumentParser()
     parser.add_argument("--test-scores", action="store_true")
     parser.add_argument("--size", type=int, default=1)
     parser.add_argument("--dest", type=str, default="dataset.pt")
     args = parser.parse_args()
 
-    def signal_handler(sig, frame):
-        print("\nInterrupt received, saving dataset...")
-        if global_dataset is not None:
-            global_dataset.save(args.dest)
-        sys.exit(0)
-
-    signal.signal(signal.SIGINT, signal_handler)
-
     if args.test_scores:
         print("====== Testing scores:")
         score_tester()
 
     print("====== Generating dataset:")
-    global_dataset = NodeDataset.from_generation(args.size, tqdm_disable=False)
-    
-    save_interval = 10  # Save every 10 entries
-    for i, entry in enumerate(global_dataset):
-        if i % save_interval == 0:
-            global_dataset.save(args.dest)
-        print(entry)
-
-    print(f"Generated {len(global_dataset)} entries")
-    global_dataset.save(args.dest)
+    generate_dataset(args.size, args.dest)
 
 if __name__ == "__main__":
     main()
--- src/gen_parallel_dataset.py
+++ src/gen_parallel_dataset.py
@@ -4,27 +4,25 @@ import sys
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import argparse
 
-parser = argparse.ArgumentParser(description='Generate datasets in parallel.')
-parser.add_argument('duration', type=int, help='Duration in minutes for dataset generation.')
-parser.add_argument('--dest', type=str, default='datasets', help='Destination directory to save datasets.')
-args = parser.parse_args()
+from gen_dataset import generate_dataset
 
-# ENV:
-# MAX_CORES: Number of cores to use for dataset generation
-max_cores = os.environ.get("MAX_CORES") or 4
-max_cores = int(max_cores)
+def main():
+    parser = argparse.ArgumentParser(description='Generate datasets in parallel.')
+    parser.add_argument('duration', type=int, help='Duration in minutes for dataset generation.')
+    parser.add_argument('--dest', type=str, default='datasets', help='Destination directory to save datasets.')
+    args = parser.parse_args()
 
-def generate_dataset(duration, size, dest, core_num):
-    dataset_filename = f"{dest}_core_{core_num}.pt"
-    command = f"python src/gen_dataset.py --size {size} --dest {dataset_filename}"
-    os.system(command)
-    return f"Dataset of size {size} generated in {duration} minutes and saved as {dataset_filename}."
-
-if __name__ == "__main__":
+    # ENV:
+    # MAX_CORES: Number of cores to use for dataset generation
+    max_cores = os.environ.get("MAX_CORES") or 4
+    max_cores = int(max_cores)
     duration_in_minutes = args.duration
     dest_directory = args.dest
     seconds_per_entry = 12  # Time to generate one entry
-    size = duration_in_minutes * 60 // seconds_per_entry
+    size = 1 # duration_in_minutes * 60 // seconds_per_entry
+
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
 
     print(f"Generating datasets of size {size} on {max_cores} cores for {duration_in_minutes} minutes...", file=sys.stderr)
 
@@ -32,9 +30,13 @@ if __name__ == "__main__":
 
     with ProcessPoolExecutor(max_workers=max_cores) as executor:
         for core_num in range(max_cores):
-            future = executor.submit(generate_dataset, duration_in_minutes, size, dest_directory, core_num)
+            dest = os.path.join(dest_directory, f"dataset-{core_num}.pt")
+            future = executor.submit(generate_dataset, size, dest)
             futures.add(future)
 
         for future in as_completed(futures):
             result = future.result()
-            print(result, file=sys.stderr)
+            print("================= future done res:", result, file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
