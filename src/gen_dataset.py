f396a00a112b69a44942390510908932893f4c54
quintenbons <quintenbons@outlook.com>
2023-12-13 18:05:34 +0100

    Better gen_dataset script

--- src/gen_dataset.py
+++ src/gen_dataset.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-from ai.dataset import NodeDataset
+from ai.dataset import NodeDataset, score_tester
+import argparse
 
 def main():
-    ds = NodeDataset.from_generation(1, tqdm_disable=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test-scores", action="store_true")
+    parser.add_argument("--size", type=int, default=1)
+    args = parser.parse_args()
+
+    if args.test_scores:
+        print("====== Testing scores:")
+        score_tester()
+
+    print("====== Generating dataset:")
+    ds = NodeDataset.from_generation(args.size, tqdm_disable=False)
     print(f"Generated {len(ds)} entries")
 
     for entry in ds:
