bf2d18d76951d7a2a4a1c7e4f15141a49ab864a9
quintenbons <quintenbons@outlook.com>
2023-12-14 20:17:16 +0100

    Sticky nav

--- react_app/src/components/NavBar.tsx
+++ react_app/src/components/NavBar.tsx
@@ -11,42 +11,52 @@ export type NavBarProps = {
 export const NavBar = (props: NavBarProps) => {
   const { colorMode } = useColorMode();
   return (
-    <Box
-      sx={{
-        width: "100%",
-        backgroundColor:
-          colorMode === "light"
-            ? theme.colors.green[100]
-            : theme.colors.green[900],
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        height: theme.sizes[16],
-        gap: theme.space[7],
-        padding: theme.space[4],
-      }}
-    >
-      <Icon w={8} h={8}>
-        Icon
-      </Icon>
-      {props.sections.map((section) => (
-        <Link
-          sx={{
-            verticalAlign: "middle",
-            textAlign: "center",
-          }}
-          href={section.href}
-        >
-          {section.name}
-        </Link>
-      ))}
+    <>
       <Box
         sx={{
-          marginLeft: "auto",
+          width: "100vw",
+          backgroundColor:
+            colorMode === "light"
+              ? theme.colors.green[100]
+              : theme.colors.green[900],
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          height: theme.sizes[16],
+          gap: theme.space[7],
+          padding: theme.space[4],
+          position: "fixed",
         }}
       >
-        <ColorSwitch />
+        <Icon w={8} h={8}>
+          Icon
+        </Icon>
+        {props.sections.map((section) => (
+          <Link
+            sx={{
+              verticalAlign: "middle",
+              textAlign: "center",
+            }}
+            href={section.href}
+          >
+            {section.name}
+          </Link>
+        ))}
+        <Box
+          sx={{
+            marginLeft: "auto",
+          }}
+        >
+          <ColorSwitch />
+        </Box>
       </Box>
-    </Box>
+      <Box
+        sx={{
+          width: "100vw",
+          height: theme.sizes[16],
+          padding: theme.space[4],
+        }}
+      />
+    </>
   );
 };
