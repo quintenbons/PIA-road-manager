d522acdf1951b5c89e3f1a8cd4f468aa22ed556f
quintenbons <quintenbons@outlook.com>
2023-12-14 18:49:09 +0100

    Bench AI

--- src/bench_ai.py
+++ src/bench_ai.py
@@ -4,7 +4,8 @@ import argparse
 import os
 
 from torch.utils.data import DataLoader
-from ai.training import train
+from ai.dataset import NodeDataset
+from ai.model import CrossRoadModel
 
 def main():
     parser = argparse.ArgumentParser()
@@ -13,18 +14,30 @@ def main():
     args = parser.parse_args()
 
     if not os.path.exists(args.model_path) or not os.path.exists(args.dataset_path):
-        print(f"Dataset or model missing {args.model_path} {args.dataset_path}}")
+        print(f"Dataset or model missing {args.model_path} {args.dataset_path}")
         exit(1)
 
-    model = torch.load(args.model_path)
-    dataset = torch.load(args.dataset_path)
+    model = CrossRoadModel.load(args.model_path)
+    dataset = NodeDataset.load(args.dataset_path)
     dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
     print("+++++++++++++++++++ This is not totally working ++++++++++++++++++++++")
 
+    hit = 0
+    miss = 0
+
     with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(dataloader):
-            print(model(inputs))
+        for batch_idx, (inputs, expected) in enumerate(dataloader):
+            results = model(inputs)
+            for output, target in zip(results, expected):
+                hitormiss = "\033[92mHIT \033[0m" if output.argmax().item() == target.argmax().item() else "\033[91mMISS\033[0m"
+                print(f"{hitormiss} Got: {output.argmax().item():2d} Expected: {target.argmax().item():2d}")
+                if output.argmax().item() == target.argmax().item():
+                    hit += 1
+                else:
+                    miss += 1
+        print()
+        print(f"HIT rate: {hit / (hit + miss) * 100:.1f}% ({hit}/{hit+miss})")
 
 if __name__ == "__main__":
     main()
