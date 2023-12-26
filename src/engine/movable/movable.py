60aa5b29748c3cba591b57f3477c357458ea4b8d
Luca Bitaudeau <luca.bitaudeau@grenoble-inp.org>
2023-12-27 00:56:34 +0100

    ajout de l'interface pour les movables, faut ensuite faire la même avec les autres objets

--- src/engine/movable/movable.py
+++ src/engine/movable/movable.py
@@ -30,7 +30,21 @@ class Movable(Nodable):
     def __init__(self, speed, acceleration, pos, size, spawn_tick: int = 0):
 
         self.cmovable = engine_ia.Movable(speed, acceleration, pos, size, spawn_tick, TIME)
- 
+    
+    def get_score(self, current_tick):
+        return self.cmovable.get_score(current_tick)
+    
+    def get_pos(self):
+        return self.cmovable.get_pos()
+    
+    def set_road_goal(self, destination: Road, pos: float):
+        self.cmovable.set_road_goal(destination.croad, pos)
+
+    def get_id(self):
+        self.cmovable.get_id()
+    
+    def to_coord_xy(self):
+        self.cmovable.to_coord_xy()
     # def __str__(self):
     #     return f'{{(x,y): {self.to_coord_xy()}, "pos on the road": {self.pos}, "speed": {self.speed}, "latency": {self.latency}, "size": {self.size}, "node": {self.node} "road": {self.road}, "id": {self._id}}}'
     def __str__(self):
