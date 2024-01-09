13d109e16e7963cb53115a53c4c1a41b77c9d66d
quintenbons <quintenbons@outlook.com>
2024-01-09 15:43:47 +0100

    Better parralel generation

--- src/gen_parallel_dataset.py
+++ src/gen_parallel_dataset.py
@@ -16,7 +16,9 @@ def main():
     parser = argparse.ArgumentParser(description='Generate datasets in parallel.')
     parser.add_argument('size', type=int, help='Size of dataset generation')
     parser.add_argument('--dest', type=str, default='datasets', help='Destination directory to save datasets.')
+    parser.add_argument('--verbose', action="store_true", help='Enable if you need to output the dataset entries in logs.')
     parser.add_argument("--map_folder", type=str, default="src/maps/build/GUI/Training-4/Uniform", help="Folder containing map.csv and paths.csv")
+    parser.add_argument("--cores", type=int, default=-1, help="Max cores. -1 for all cores (default)")
     args = parser.parse_args()
 
     if os.path.exists(args.dest):
@@ -24,15 +26,18 @@ def main():
         if answer.lower() != "y":
             print("Aborting")
             exit(1)
+        os.system(f"rm -rf {args.dest}")
 
     # ENV:
     # MAX_CORES: Number of cores to use for dataset generation
-    max_cores = os.environ.get("MAX_CORES") or 8
-    max_cores = int(max_cores)
+    max_cores = args.cores
+    if max_cores == -1:
+        max_cores = os.cpu_count()
     size = args.size
     dest_directory = args.dest
     seconds_per_entry = 1  # Time to generate one entry
     approx_duration = size * seconds_per_entry / 60
+    quiet = not args.verbose
 
     if not os.path.exists(dest_directory):
         os.makedirs(dest_directory)
@@ -46,7 +51,7 @@ def main():
     with ProcessPoolExecutor(max_workers=max_cores) as executor:
         for core_num in range(max_cores):
             dest = os.path.join(dest_directory, f"dataset-{core_num}.pt")
-            future = executor.submit(generate_dataset_seeded, base_seed + core_num, size, dest, args.map_folder)
+            future = executor.submit(generate_dataset_seeded, base_seed + core_num, size, dest, args.map_folder, False, quiet)
             futures.add(future)
 
         for future in as_completed(futures):
