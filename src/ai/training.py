375f0844761d3e6876c78e6763a08f6d09f8461e
quintenbons <quintenbons@outlook.com>
2024-01-10 12:42:16 +0100

    better training

--- src/ai/training.py
+++ src/ai/training.py
@@ -4,11 +4,13 @@ from torch.utils.data import DataLoader
 from ai.dataset import NodeDataset
 from ai.model import CrossRoadModel
 import torch.nn as nn
+from torch.optim.lr_scheduler import StepLR
 
 def training_loop(model: CrossRoadModel, dataloader: DataLoader, num_epochs: int):
     log_interval = 100
     criterion_next_move = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
     for epoch in range(num_epochs):
         for batch_idx, (inputs, targets) in enumerate(dataloader):
             optimizer.zero_grad()
@@ -27,10 +29,12 @@ def training_loop(model: CrossRoadModel, dataloader: DataLoader, num_epochs: int
 
             if batch_idx % log_interval == 0:
                 print(f"Epoch: {epoch+1:4} / {num_epochs:4}, Batch: {batch_idx+1:8d} / {len(dataloader):8d}, Loss: {loss.item():.4f}")
+        scheduler.step()
 
 def train(dataset_target: PathLike, model_target: PathLike, num_epochs):
-    dataset = NodeDataset.load(dataset_target)
-    model = CrossRoadModel.load(model_target)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    dataset = NodeDataset.load(dataset_target, device)
+    model = CrossRoadModel.load(model_target, device)
     dataloader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4, pin_memory=True)
     training_loop(model, dataloader, num_epochs)
     model.save(model_target)
